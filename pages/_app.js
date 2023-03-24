f63fa56dfaaf047446cd0903f377e7aee75b6bc7
Hardik Desai <124128351+hardikd-zignuts@users.noreply.github.com>
2023-03-24 10:56:45 +0530

    refactor: :recycle: Remove Unnesasery Code

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
+import AppBar from '@/components/AppBar';
 import '@/styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <AppBar />
+      <Component {...pageProps} />
+    </>
+  )
 }
